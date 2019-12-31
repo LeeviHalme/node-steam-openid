7b5846085b44e845ba9b90ea1f7eec6233d2fd6a
BuildTools <unconfigured@null.spigotmc.org>
2019-12-31 14:28:26 +0200

    Added README

--- README.md
+++ README.md
@@ -1,2 +1,107 @@
 # steam-oauth
-A wrapper package around Steam's oAuth API. Supports promises :)
+
+A wrapper package around Steam's oAuth API, which supports promises :)
+
+## Usage
+
+Install the package by typing `npm i steam-oauth` in your project folder.
+
+### Setup
+
+```javascript
+const SteamAuth = require("steam-oauth");
+
+const steam = new SteamAuth({
+  realm: "http://localhost:5000", // Site name displayed to users on logon
+  returnUrl: "http://localhost:5000/auth/steam/authenticate", // Your return route
+  apiKey: "XXXXXXXXXXXXXXXXXXXXXXXXXX" // Steam API key
+});
+```
+
+### Routes
+
+```javascript
+app.get("/auth/steam", async (req, res) => {
+  const redirectUrl = await steam.getRedirectUrl();
+  return res.redirect(redirectUrl);
+});
+
+app.get("/auth/steam/authenticate", async (req, res) => {
+  try {
+    const user = await steam.authenticate(req);
+
+    //...do something with the data
+  } catch (error) {
+    console.error(error);
+  }
+});
+```
+
+## Methods
+
+### getRedirectUrl
+
+Gets the redirect URL to Steam.
+
+#### Parameters
+
+None
+
+#### Returns
+
+- Promise (String)
+
+#### Example
+
+```javascript
+steam.getRedirectUrl().then(url => {
+  //...do something with the url
+});
+```
+
+### authenticate
+
+Authenticates the user with oAuth.
+
+#### Parameters
+
+- request (ExpressJsRequest, Object)
+
+#### Returns
+
+- Promise (UserObject)
+
+#### Example
+
+```javascript
+steam.authenticate(req).then(user => {
+  //...do something with the user
+});
+```
+
+## Objects
+
+### UserObject
+
+Object which holds all the authenticated user's data. The key `_json` holds the raw response from Steam API.
+
+#### Example
+
+```javascript
+{
+  _json: { ... },
+  steamid: "12345678912345678",
+  username: "Example Username",
+  name: "Example Name",
+  profile: "https://steamcommunity.com/id/Example",
+  avatar: {
+    small: "...",
+    medium: "...",
+    large: "..."
+  }
+}
+```
+
+## License
+
+MIT <3
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const openid = require("openid");
 
 // Main Class
 class SteamAuth {
-  constructor({}) {
+  constructor({ realm, returnUrl, apiKey }) {
     if (!realm || !returnUrl || !apiKey)
       throw new Error(
         "Missing realm, returnURL or apiKey parameter(s). These are required."
@@ -25,33 +25,45 @@ class SteamAuth {
   // Get redirect url for Steam
   async getRedirectUrl() {
     return new Promise((resolve, reject) => {
-      relyingParty.authenticate("https://steamcommunity.com/openid", false, (error, authUrl) => {
-        if(error) return reject("Authentication failed: " + error);
-        if(!authUrl) return reject("Authentication failed.");
-        
-        resolve(authUrl);
-		  });
-    })
+      this.relyingParty.authenticate(
+        "https://steamcommunity.com/openid",
+        false,
+        (error, authUrl) => {
+          if (error) return reject("Authentication failed: " + error);
+          if (!authUrl) return reject("Authentication failed.");
+
+          resolve(authUrl);
+        }
+      );
+    });
   }
 
   // Fetch user
   async fetchIdentifier(steamOpenId) {
     return new Promise(async (resolve, reject) => {
       // Parse steamid from the url
-      const steamId = steamOpenId.replace("https://steamcommunity.com/openid/id/", "");
+      const steamId = steamOpenId.replace(
+        "https://steamcommunity.com/openid/id/",
+        ""
+      );
 
       try {
-        const response = await axios.get(`https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${this.apiKey}&steamids=${steamId}`);
-        const players = response.data && response.data.response && response.data.response.players;
-        
-        if(players && players.length > 0) {
+        const response = await axios.get(
+          `https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${this.apiKey}&steamids=${steamId}`
+        );
+        const players =
+          response.data &&
+          response.data.response &&
+          response.data.response.players;
+
+        if (players && players.length > 0) {
           // Get the player
           const player = players[0];
 
           // Return user data
           resolve({
             _json: player,
-            steamid: steamID,
+            steamid: steamId,
             username: player.personaname,
             name: player.realname,
             profile: player.profileurl,
@@ -60,27 +72,37 @@ class SteamAuth {
               medium: player.avatarmedium,
               large: player.avatarfull
             }
-          })
+          });
         } else {
           reject("No players found for the given SteamID.");
         }
       } catch (error) {
         reject("Steam server error: " + error.message);
       }
-    })
+    });
   }
 
   // Authenticate user
   async authenticate(req) {
     return new Promise((resolve, reject) => {
       // Verify assertion
-      this.relyingParty.verifyAssertion(req, (error, result) => {
-        if(error) return reject(error.message);
-        if(!result || !result.authenticated) return reject("Failed to authenticate user.");
-        if(!/^https?:\/\/steamcommunity\.com\/openid\/id\/\d+$/.test(result.claimedIdentifier)) return reject("Claimed identity is not valid.");
-      
-        const user = await this.fetchIdentifier(result.claimedIdentifier);
-        return resolve(user);
+      this.relyingParty.verifyAssertion(req, async (error, result) => {
+        if (error) return reject(error.message);
+        if (!result || !result.authenticated)
+          return reject("Failed to authenticate user.");
+        if (
+          !/^https?:\/\/steamcommunity\.com\/openid\/id\/\d+$/.test(
+            result.claimedIdentifier
+          )
+        )
+          return reject("Claimed identity is not valid.");
+
+        try {
+          const user = await this.fetchIdentifier(result.claimedIdentifier);
+          return resolve(user);
+        } catch (error) {
+          reject(error);
+        }
       });
     });
   }
